7798cf44874644d3283043c3b344bb8d3439b4cc
Chance Dickson <me@chncdcksn.com>
2017-06-05 00:50:49 -0500

    Made modal links open in a new tab

--- src/data/messages.js
+++ src/data/messages.js
@@ -4,8 +4,8 @@ export default [
     message: "Thank you for using my Symbaroum character creator! All data is \
       automatically saved, so you can close the tab or return to the menu from \
       the editor at any time. If you have \
-      any issues, email me at <a href=\"email:me@chncdcksn.com\">me@chncdcksn.com</a>\
-      or create an issue <a href=\"https://github.com/chncdcksn/symbaroum\">here</a>\
+      any issues, email me at <a href=\"email:me@chncdcksn.com\" target=\"_blank\">me@chncdcksn.com</a>\
+      or create an issue <a href=\"https://github.com/chncdcksn/symbaroum\" target=\"_blank\">here</a>\
       and I'll be sure to fix it as soon as possible. :)"
   }
 ];
